dba389f216115e3c9dcc1c4df0f95fb3c6952bb4
minwoo.cha <ds2cmw@naver.com>
2024-02-16 17:19:22 +0900

    프로젝트 시작 1-1

--- server/app.js
+++ server/app.js
@@ -3,22 +3,38 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
-const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const passport = require('passport');
 
+
 dotenv.config();
+
+const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+// 미들웨어 설정
 app.use(morgan('dev'));
+// upblic 폴더 안의 파일 사용
 app.use(express.static(path.join(__dirname, 'public')));
+// json 형태의 요청 body를 파싱하려고 사용
 app.use(express.json());
+// url 한글 인코딩 허용
 app.use(express.urlencoded({ extended: false }));
+// .env 파일의 COOKIE_SECRET 파일 사용
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(session({
+    resave : false,  // 모든 request마다 기존에 세션에 변경사항이 없을 시에도 세션을 다시 저장
+    saveUninitialized : false,  // request가 들어오면 saveUninitialized 상태의 세션을 강제로 저장 해제
+    secret : process.env.COOKIE_SECRET,  // 암호화 하는데 쓰일 키
+    cookie : {
+        httpOnly : true,  // 자바스크립트로 쿠키에 접근하지 못하게 할 때 사용
+        secure : false  // https 로만 사용하게 할 때 사용
+    }
+}));
+app.use(passport.initialize()); // passport 초기화
+app.use(passport.session());  // passport 세션 사용
 
 app.use((req, res, next) => {
     const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -26,7 +42,8 @@ app.use((req, res, next) => {
     next(error);
 });
 
-app.use((err, req, res, next) => { 
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
     res.status(err.status || 500);
     res.render('error');
